39f1d256514fa75858ff14ed2524401307d89cc6
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-01-15 14:07:13 +0300

    removed the config file

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from stark import run
 from voice_commands import manager
 from stark.interfaces.vosk import VoskSpeechRecognizer
 from stark.interfaces.silero import SileroSpeechSynthesizer
-from config import (
-    SILERO_MODEL_URL,
-    VOSK_MODEL_URL
-)
 
+SILERO_MODEL_URL = "https://models.silero.ai/models/tts/ru/v3_1_ru.pt"
+VOSK_MODEL_URL = "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip"
 recognizer = VoskSpeechRecognizer(model_url=VOSK_MODEL_URL)
 synthesizer = SileroSpeechSynthesizer(model_url=SILERO_MODEL_URL)
 
