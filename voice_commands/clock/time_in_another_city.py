f189c8b705e0b27f5f856d5d0eec4985e87214b0
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-01-20 13:30:50 +0300

    added time in city X

--- voice_commands/clock/time_in_another_city.py
+++ voice_commands/clock/time_in_another_city.py
@@ -1,29 +1,39 @@
-from dependencies.auxiliary_functions import convert, translate_city
+from dependencies.auxiliary_functions import convert
 from stark.core.types import String
 from stark import Response, CommandsManager
 import datetime
 import pytz 
-
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
 time_city = CommandsManager()
 
-@time_city.new(r"(сколько|какое|узнать|узнай|определить|определи|подскажи) время (в|для) (городе|города) $city:String")
-def get_time_in_city(city: String) -> Response:
-    """
-    Определение времени в указанном городе.
-    """
-    city_name = city.value
-    #Переводим название города с русского на английский
-    translated_city = translate_city(city=city_name).title()
+@time_city.new(r"(сколько|какое|узнать|узнай|определить|определи|подскажи) время (в|для) (городе|города)? $city:String")
+def get_current_time_in_city(city:String):
+    city_name = f"{city.value}"
+
+    # Создаем объект геокодера
+    geolocator = Nominatim(user_agent="city_time_app")
+    if " " in city_name:
+        city_name = city_name.replace(" ","-")
+    # Получаем координаты города
+    location = geolocator.geocode(city_name)
     
-    # Поиск временной зоны
-    for timezone in pytz.all_timezones:
-       # Проверяем, совпадает ли название города с одной из частей временной зоны
-        if translated_city in timezone.replace(" ", "_").split('/') or translated_city in timezone.replace("-", "_").split('/'):
-            # Если найдено совпадение, возвращаем текущее время
-            tz = pytz.timezone(timezone)
-            city_time = datetime.datetime.now(tz)
-            hour = convert(city_time.hour)
-            minute = convert(city_time.minute)
-            translated_city = translate_city(city_name,to_lang="ru",from_lang="en")
-            return Response(voice=f"Время в городе {translated_city} {hour} часов {minute} минут")
-    return Response(voice="Не удалось распознать город")
+    if location:
+        latitude = location.latitude
+        longitude = location.longitude
+        
+        # Ищем временную зону по координатам
+        timezone_finder = TimezoneFinder()
+        timezone_str = timezone_finder.timezone_at(lng=longitude, lat=latitude)
+        
+        if timezone_str:
+            # Получаем текущее время в указанной временной зоне
+            local_time = datetime.datetime.now(pytz.timezone(timezone_str))
+            hour  = convert(local_time.hour)
+            minute = convert(local_time.minute)
+            # Форматируем вывод
+            return Response(voice=f"Текущее время в {city_name}: {hour} часов {minute} минут")
+        else:
+            return Response(voice=f"Не удалось определить временную зону для города {city_name}")
+    else:
+        return Response(voice=f"Город {city_name} не найден")
