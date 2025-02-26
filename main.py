de5b058138e718024c0e514132a5f069df98bb76
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-02-26 11:41:29 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,23 @@
+import anyio
+from stark import run
+from voice_commands import data_manager, Data, Time, time_manager
+from stark.interfaces.vosk import VoskSpeechRecognizer
+from stark.interfaces.silero import SileroSpeechSynthesizer
+
+
+VOSK_MODEL_URL = "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip"
+SILERO_MODEL_URL = "https://models.silero.ai/models/tts/ru/v3_1_ru.pt"
+
+recognizer = VoskSpeechRecognizer(model_url=VOSK_MODEL_URL)
+synthesizer = SileroSpeechSynthesizer(model_url=SILERO_MODEL_URL)
+
+data_instance = Data()
+time_instance = Time()
+
+data_manager.extend(time_manager)
+
+async def main():
+    await run(data_manager, recognizer, synthesizer)
+
+if __name__ == "__main__":
+    anyio.run(main)
