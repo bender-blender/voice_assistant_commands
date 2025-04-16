05b479d1fa9005f146285369135c815d2a614a7e
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-04-16 13:28:00 +0300

    embedded a note into the system

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ from voice_commands import (
     timer_manager,
     State,
     Stopwatch,
-    stopwatch_manager)
+    stopwatch_manager,
+    Note,
+    note_manager)
 from stark.interfaces.vosk import VoskSpeechRecognizer
 from stark.interfaces.silero import SileroSpeechSynthesizer
-
+from stark import Response
 
 VOSK_MODEL_URL = "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip"
 SILERO_MODEL_URL = "https://models.silero.ai/models/tts/ru/v3_1_ru.pt"
@@ -31,6 +33,7 @@ data_instance = Data()
 city = CityTime()
 time_instance = Time()
 state = State()
+note = Note()
 stopwatch = Stopwatch()
 media = MediaPlayer()
 request = Request()
@@ -38,13 +41,23 @@ request = Request()
 data_manager.extend(timer_manager)
 data_manager.extend(time_city_manager)
 data_manager.extend(time_manager)
+data_manager.extend(note_manager)
 data_manager.extend(stopwatch_manager)
 data_manager.extend(media_manager)
 data_manager.extend(request_manager)
 
+async def handle_reminders():
+    async for response in note.reminder_loop():
+        speech = await synthesizer.synthesize(response.voice)
+        await speech.play()
+
 
 async def main():
-    await run(data_manager, recognizer, synthesizer)
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(handle_reminders)
+        tg.start_soon(run, data_manager, recognizer, synthesizer)
 
 if __name__ == "__main__":
     anyio.run(main)
+
+
