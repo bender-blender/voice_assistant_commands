5d6f104bb8ba50282fbef21904f54d31b792887e
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-05-20 12:32:24 +0300

    added time provider to main file

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import anyio
 from stark import run
 from voice_commands import (
-    time_manager,
-    timer_manager,
-    media_manager,
-    request_manager,
-    note_manager,
-    note,
-    weather_manager)
+    time)
 from stark.interfaces.vosk import VoskSpeechRecognizer
 from stark.interfaces.silero import SileroSpeechSynthesizer
 
@@ -18,21 +12,17 @@ SILERO_MODEL_URL = "https://models.silero.ai/models/tts/ru/v3_1_ru.pt"
 recognizer = VoskSpeechRecognizer(model_url=VOSK_MODEL_URL)
 synthesizer = SileroSpeechSynthesizer(model_url=SILERO_MODEL_URL)
 
-time_manager.extend(timer_manager)
-time_manager.extend(media_manager)
-time_manager.extend(request_manager)
-time_manager.extend(weather_manager)
-time_manager.extend(note_manager)
+
 
 async def main():
     async with anyio.create_task_group() as tg:
-        tg.start_soon(run, time_manager, recognizer, synthesizer)
-        tg.start_soon(handle_reminders)
+        tg.start_soon(run, time, recognizer, synthesizer)
+#         tg.start_soon(handle_reminders)
 
-async def handle_reminders():
-    async for response in note.reminder_loop():
-        speech = await synthesizer.synthesize(response.voice)
-        await speech.play()
+# async def handle_reminders():
+#     async for response in note.reminder_loop():
+#         speech = await synthesizer.synthesize(response.voice)
+#         await speech.play()
 
 if __name__ == "__main__":
     anyio.run(main)
