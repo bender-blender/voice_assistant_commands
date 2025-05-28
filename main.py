bedf0f3c470dd13ab92913a41c2c9f598f55badb
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-05-28 17:11:41 +0300

    added a reminder to main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from stark import run
 from voice_commands import (
     time_manager,
     stopwatch_manager,
-    reminder_manager)
+    reminder_manager,
+    reminder)
 from stark.interfaces.vosk import VoskSpeechRecognizer
 from stark.interfaces.silero import SileroSpeechSynthesizer
 from dotenv import load_dotenv
@@ -24,12 +25,12 @@ reminder_manager.extend(time_manager)
 async def main():
     async with anyio.create_task_group() as tg:
         tg.start_soon(run, reminder_manager, recognizer, synthesizer)
-#         tg.start_soon(handle_reminders)
+        tg.start_soon(handle_reminders)
 
-# async def handle_reminders():
-#     async for response in note.reminder_loop():
-#         speech = await synthesizer.synthesize(response.voice)
-#         await speech.play()
+async def handle_reminders():
+    async for response in reminder.reminder_loop():
+        speech = await synthesizer.synthesize(response.voice)
+        await speech.play()
 
 if __name__ == "__main__":
     anyio.run(main)
