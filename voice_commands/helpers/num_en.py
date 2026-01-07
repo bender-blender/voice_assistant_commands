cf2f8ecbdd97ca885d80f7fa6a4451b5498bd2ab
Maxim <maksimgarsia709@gmail.com>
2026-01-07 18:42:04 +0200

    added additional helper functions

---
+++ voice_commands/helpers/num_en.py
@@ -0,0 +1,5 @@
+fraction_en = {"quarter": 0.25, "a quarter": 0.25, "one quarter": 0.25,
+            "third": 0.333, "a third": 0.333, "one third": 0.333,
+            "fourth": 0.25, "a fourth": 0.25, "one fourth": 0.25, }
+
+half_en = {"half": 0.5, "a half": 0.5, "one half": 0.5}
