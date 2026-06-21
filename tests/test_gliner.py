cc9a52dddcc6593edc7130a1d8baff10b7c1c662
Maxim <maksimgarsia709@gmail.com>
2026-06-21 14:38:18 +0300

    Checking if a list contains all required objects

--- tests/test_gliner.py
+++ tests/test_gliner.py
@@ -1,6 +1,7 @@
 from stark.core.commands_context import CommandsContext
 from utilits.gliner_processor import GliNERProcessor
 from stark.core.parsing import RecognizedEntity
+from stark.core.types.location import Location
 from typing import cast
 import pytest
 
@@ -15,24 +16,29 @@ def ner():
 @pytest.mark.parametrize(
     "text, obj",
     [
-        ("Я живу в Киеве", ["киеве"]),
-        ("Переехал в Харьков пару лет назад", ["харьков"]),
-        ("Мы летим в Одессу завтра утром", ["одессу"]),
-        ("Конференция проходила во Львове", ["львове"]),
-        ("Он работает в Днепре", ["днепре"]),
-        ("Переехал в Франкфурт на Одере пару лет назад",["франкфурт","одере"]),
-        ("Вызови убер в Киев",["убер","киев"]),
-        ("Какая погода в Сан Франциско",["сан франциско"]),
-        ("Аэропорт Шереметьево далеко?", ["аэропорт шереметьево"]),
-        ("Время в городе Париж",["париж"]),
-        ("Подскажи где тут ближайший магазин АТБ",["атб"]),
-        ("Путь к Сильпо",["сильпо"]),
-        ("Где ближайший банкомат ПриватБанка", ["приватбанка"]),
-        ("Построй маршрут к Макдональдсу", ["макдональдсу"]),
+        ("Я живу в Киеве", [Location("киеве")]),
+        ("Переехал в Харьков пару лет назад", [Location("харьков")]),
+        ("Мы летим в Одессу завтра утром", [Location("одессу")]),
+        ("Конференция проходила во Львове", [Location("львове")]),
+        ("Он работает в Днепре", [Location("днепре")]),
+        ("Переехал в Франкфурт на Одере пару лет назад",[Location("франкфурт"),Location("одере")]),
+        ("Вызови убер в Киев",[Location("убер"),Location("киев")]),
+        ("Какая погода в Сан Франциско",[Location("сан франциско")]),
+        ("Аэропорт Шереметьево далеко?", [Location("аэропорт шереметьево")]),
+        ("Время в городе Париж",[Location("париж")]),
+        ("Подскажи где тут ближайший магазин АТБ",[Location("атб")]),
+        ("Путь к Сильпо",[Location("сильпо")]),
+        ("Где ближайший банкомат ПриватБанка", [Location("приватбанка")]),
+        ("Построй маршрут к Макдональдсу", [Location("макдональдсу")]),
     ],
 )
 async def test_city_extraction(text: str, obj: list[str], ner:GliNERProcessor):
     recognized_entities: list[RecognizedEntity] = []
     await ner.process_string(text.lower(), cast(CommandsContext, None), recognized_entities)
-    for i in recognized_entities:
-        assert i.substring in obj
+    
+
+    lst = [Location(e.substring) for e in recognized_entities]
+    assert lst == obj
+    #assert recognized_entities == obj
+    # for i in recognized_entities:
+    #     assert i.substring in obj
