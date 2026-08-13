514472449f37c0d673391efe89dcab7f04445e55
Maxim <maksimgarsia709@gmail.com>
2026-08-13 17:56:17 +0300

    Changed duckling to rus2num and number_parser

--- voice_commands/nl_types/nl_number/num_en.py
+++
@@ -1,5 +0,0 @@
-part_en = {"quarter": 0.25, "a quarter": 0.25, "one quarter": 0.25, "quarters":0.25,
-            "third": 0.333, "a third": 0.333, "one third": 0.333,
-            "fourth": 0.25, "a fourth": 0.25, "one fourth": 0.25, }
-
-half_en = {"half": 0.5, "a half": 0.5, "one half": 0.5}
